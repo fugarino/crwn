ea91373916b6bb8067a75f140dce0addbd0bbac0
Christian Fugarino <christian@Christians-MacBook-Pro.local>
2021-11-27 10:38:54 -0500

    homepage layout

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,2 +1,41 @@
 import React from "react";
-import "./homepage.styles.scss";
+import "./homepage.component.scss";
+
+export const HomePage = () => (
+  <div>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="directory-item">
+          <div className="content">
+            <h1 className="title">ITEM 1</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="directory-item">
+          <div className="content">
+            <h1 className="title">ITEM 2</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="directory-item">
+          <div className="content">
+            <h1 className="title">ITEM 3</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="directory-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="directory-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
